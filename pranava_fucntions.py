b07af0f28a55070a0eadeb109c544c153084e3b7
realpranav93 <pranavbalasankula@gmail.com>
2022-05-25 17:50:17 +0530

    added get_fiscal_quarter_ms function to the pranava_functions

--- pranava_fucntions.py
+++ pranava_fucntions.py
@@ -13,3 +13,28 @@ def remove_nan_list(l):
     description : removes a nan from the list and returns the final list
     """
     return [a for a in l if a == a ]
+
+
+def get_fiscal_quarter_ms(x, offset = 0):
+    """
+    x: date,
+     
+    offset  -- measure to offset the quarter to previous or future quarters (default=1)
+
+    ouptut: Returns a microsoft fiscal quarter number from a given date in format fyqq e.g. 202101 for FY21 quarter1
+    """ 
+    from pandas.tseries.offsets import DateOffset,BQuarterEnd
+    if isinstance(x, datetime.datetime):
+        if offset > 0: 
+            offset += 1
+        
+        print(offset)
+        f_x = x + BQuarterEnd(offset)
+        print(x, f_x)
+        
+        fiscal_quarter = f_x.to_period('Q-Jul')
+
+    else: 
+        print("Input is not a date") 
+
+    return fiscal_quarter
